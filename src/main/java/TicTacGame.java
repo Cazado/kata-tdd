7d13f5075db0c9b5ad7768f64db519170c253b2a
Emanuel Suca <esuca@autentia.com>
2019-04-23 22:18:43 +0200

    adds TicTac game comment

--- src/main/java/TicTacGame.java
+++ src/main/java/TicTacGame.java
@@ -1,4 +1,14 @@
+
+/*
+	Escribir un programa que muestre en pantalla
+
+	Los números del 1 al 100.
+	Sustituye los múltiplos de 3 por la palabra “Tic”
+	Sustituye los múltiplos de 5 por la palabra “Tac”
+	Sustituye los múltiplos de 3 y 5 por la palabra “TicTac”
+*/
 class TicTacGame {
+
 
 	String getNumber(int number) {
 		return String.valueOf(number);
